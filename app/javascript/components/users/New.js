6121f71e89909e5fa852426ab3cbac2ad0466944
Abhinav Singh <singh.abhinav6996@gmail.com>
2020-06-04 18:38:47 +0530

    Remove users controller

---
+++ app/javascript/components/users/New.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+class New extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>hello</h1>
+      </React.Fragment>
+    );
+  }
+}
+
+export default New;
