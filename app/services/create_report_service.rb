84b3d3e348b3a473993fc10924a409f725bc8028
Abhinav Singh <singh.abhinav6996@gmail.com>
2020-06-25 18:09:21 +0530

    Add filename path

--- app/services/create_report_service.rb
+++ app/services/create_report_service.rb
@@ -10,5 +10,6 @@ class CreateReportService < ApplicationService
     current_user = User.find(@user_id)
     quizzes = current_user.quizzes.published.eager_load(submitted_attempts: :user)
     file_name = Time.now.strftime("%Y-%m-%d_%H-%M-%S") + "_report.csv"
+    file_path = "#{Rails.root}/public/#{file_name}"
   end
 end
