5d0ae457492e2dbe5cd5aec6eaee77c03797421e
Abhinav Singh <singh.abhinav6996@gmail.com>
2020-06-26 12:17:58 +0530

    Add csv write file method

--- app/services/create_report_service.rb
+++ app/services/create_report_service.rb
@@ -11,5 +11,19 @@ class CreateReportService < ApplicationService
     quizzes = current_user.quizzes.published.eager_load(submitted_attempts: :user)
     file_name = Time.now.strftime("%Y-%m-%d_%H-%M-%S") + "_report.csv"
     file_path = "#{Rails.root}/public/#{file_name}"
+
+    CSV.open(file_path, "wb") do |csv|
+      csv << HEADER_ROW
+      quizzes.each do |quiz|
+        quiz.submitted_attempts.each do |attempt|
+          csv << [quiz.name,
+                "#{attempt.user.first_name} #{attempt.user.last_name}",
+                attempt.user.email,
+                attempt.correct_answers_count,
+                attempt.incorrect_answers_count]
+        end
+      end
+    end
+    job = current_user.jobs.create(job_id: @job_id, filename: file_path, status: "done")
   end
 end
