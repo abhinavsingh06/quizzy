f275260649c7a297008280eb6b2da50b28a6b687
Abhinav Singh <singh.abhinav6996@gmail.com>
2020-07-01 22:07:48 +0530

    Initialize hash of Items and SaleItems for inventory

--- challenge-price-calculator/price-calculator.rb
+++ challenge-price-calculator/price-calculator.rb
@@ -1,3 +1,25 @@
+class Items
+  @items = {}
+  def initialize(name, price)
+    @items[name] = price
+  end
+
+  def self.all
+    @items
+  end
+end
+
+class SaleItems
+  @sale_items = {}
+  def initialize(name, units, price)
+    @sale_items[name] = { 'units' => units, 'price' => price }
+  end
+
+  def self.all
+    @sale_items
+  end
+end
+
 class PriceCalculator
   def get_input
     puts "Please enter all the items purchased separated by a comma"
