f8136a29efc120051b05ccb6456861113c654f74
Abhinav Singh <singh.abhinav6996@gmail.com>
2020-07-01 17:35:42 +0530

    Add method to display table structure.

--- challenge-price-calculator/price-calculator.rb
+++ challenge-price-calculator/price-calculator.rb
@@ -3,4 +3,11 @@ class PriceCalculator
     puts "Please enter all the items purchased separated by a comma"
     response = gets.chomp
   end
+  
+  def display_bill
+    puts "Item     Quantity      Price"
+    puts "--------------------------------------"
+    puts "Total price : #{calculated price}"
+    puts "You saved #{saved_price} today."
+  end
 end
