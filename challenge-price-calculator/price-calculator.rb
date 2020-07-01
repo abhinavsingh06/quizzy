1410222f6260bb11b0b561b66685347e2475340e
Abhinav Singh <singh.abhinav6996@gmail.com>
2020-07-02 01:21:18 +0530

    Add generate bill method to create bill

--- challenge-price-calculator/price-calculator.rb
+++ challenge-price-calculator/price-calculator.rb
@@ -21,6 +21,20 @@ class SaleItems
 end
 
 class PriceCalculator
+  def generate_bill
+    input = get_input.split(',').map(&:strip)
+    @purchased_items = input
+    if purchased_items.empty?
+      puts "First add items to generate bill"
+    else 
+      price = calculate_bill(quantity)
+      billing_items = quantity.each_with_object(price) { |(key,value), billing_items| 
+        billing_items[key] = {'units' => value, 'price' => price[key]} 
+      }
+      display_bill(billing_items, quantity)
+    end
+  end
+
   def get_input
     puts "Please enter all the items purchased separated by a comma"
     response = gets.chomp
@@ -30,7 +44,7 @@ class PriceCalculator
     quantity.map { |item,value| [item, SalesItems.all[item].nil? ? quantity[item]*Items.all[item] : (((quantity[item]/SaleItems.all[item]['units']).floor)*SaleItems.all[item]['price']) + ((quantity[item]%SaleItems.all[item]['units'])*Items.all[item])] }.to_h
   end
   
-  def display_bill
+  def display_bill(billing_items, quantity)
     puts "Item     Quantity      Price"
     puts "--------------------------------------"
     puts "Total price : #{calculated price}"
