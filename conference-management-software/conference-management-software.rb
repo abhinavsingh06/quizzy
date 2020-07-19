f28b654b3b12c6cd2826406042cc2e6f46a8bc82
Abhinav Singh <singh.abhinav6996@gmail.com>
2020-07-19 12:06:52 +0530

    Added class ConferenceManager

--- conference-management-software/conference-management-software.rb
+++ conference-management-software/conference-management-software.rb
@@ -40,3 +40,95 @@ class Session
       length == 5 ? "talk" : "#{length}minutes"
     end
 end
+
+class Talk
+  attr_reader :title
+  attr_reader :length
+
+  LIGHTNING_TALK_LENGTH = 5
+
+  def initialize(talk)
+    @title, @length = *title_and_length(talk)
+  end
+
+  def self.quantity_of_tracks(talk_lists)
+    total_length_of_talks = talk_lists.values.reduce(:+)
+
+    total_length_of_sessions = Session::MORNING_LENGTH + Session::AFTERNOON_LENGTH
+    (total_length_of_talks / total_length_of_sessions.to_f).ceil
+  end
+
+  def self.update_talk_lists(talk_lists, track)
+    track.morning_sessions.talks.each do |talk|
+      talk = talk[/(?=\s).*(?=\s)/].strip
+      talk_lists.delete_if {|key, value| key == talk }
+    end
+    
+    track.afternoon_sessions.talks.each do |talk|
+      talk = talk[/(?=\s).*(?=\s)/].strip
+      talk_lists.delete_if {|key, value| key == talk }
+    end
+    talk_lists
+  end
+
+  private
+
+    def title_and_length(talk)
+      title = talk[/.*(?=\s)/]
+      str_length = talk.split
+      if str_length.last.downcase == "talk"
+        length = LIGHTNING_TALK_LENGTH
+      elsif str_length != "talk"
+        length = str_length[-2..-1].join.gsub!("minutes", ' ').to_i
+      else
+        fail ArgumentError, "invalid talk length"
+      end
+      [title, length]
+    end
+end
+
+class Track
+  attr_reader :morning_sessions
+  attr_reader :afternoon_sessions
+
+  def initialize
+    @morning_sessions   = Session.new("morning")
+    @afternoon_sessions = Session.new("afternoon")
+  end
+
+  def insert_talks_on_track(talk_lists)
+    talk_lists.each do |title, length|
+      if @morning_sessions.has_space?(length)
+        @morning_sessions.add(title, length)
+      elsif @afternoon_sessions.has_space?(length)
+        @afternoon_sessions.add(title, length)
+      end
+    end
+  end
+end
+
+class ConferenceManager
+  file = "#{Dir.pwd}/talks.txt"
+  talk_lists = {}
+  File.readlines(file).each do |line|
+    talk = Talk.new(line.strip)
+    talk_lists[talk.title] = talk.length
+  end
+  
+  number_of_tracks_needed = Talk.quantity_of_tracks(talk_lists)
+  number_of_tracks_needed.times do |n|
+    puts "TRACK #{n + 1}"
+    track = Track.new
+  
+    track.insert_talks_on_track(talk_lists)
+    talk_lists = Talk.update_talk_lists(talk_lists, track)
+  
+    puts "- MORNING SESSION"
+    puts track.morning_sessions.talks
+    puts "12:00 PM Lunch"
+    puts "- AFTERNOON SESSION"
+    puts track.afternoon_sessions.talks
+    puts "17:00 PM Networking Event"
+    puts "==============================="
+  end
+end
